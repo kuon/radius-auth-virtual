1ac9c72968771e3a3e972deb49330c985cf0f564
Nicolas goy <kuon@goyman.com>
2020-08-10 01:16:08 +0200

    More test

--- radius_virtual/tests/radius_test.rs
+++ radius_virtual/tests/radius_test.rs
@@ -1,10 +1,27 @@
+#[macro_use] extern crate assert_matches;
+
 use radius_virtual::prelude::*;
+
 
 #[test]
 fn it_passes_auth() -> Result<(), Error> {
     let c = client()?;
     let cred = Credentials::with_username_password("testing", "password");
     let user = c.authenticate(&cred)?;
+    assert_eq!(user.attributes.len(), 1);
+    assert_eq!(user.attributes[0].vendor, 1);
+    assert_eq!(user.attributes[0].subtype, 1);
+    assert_eq!(user.attributes[0].data, vec![0xAA]);
+    Ok(())
+}
+
+
+#[test]
+fn it_fails_auth() -> Result<(), Error> {
+    let c = client()?;
+    let cred = Credentials::with_username_password("testing", "fail");
+    let res = c.authenticate(&cred);
+    assert_matches!(res, Err(Error::AuthReject));
     Ok(())
 }
 
